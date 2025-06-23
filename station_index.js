2d27be96b401b0bc00a330200defdacab00b9bc2
kazu-4728 <ayabou.0930@gmail.com>
2025-06-24 08:27:45 +0900

    Add more lines and station search

---
+++ station_index.js
@@ -0,0 +1,11 @@
+const stationIndex = [
+  {station: "池袋", line: "yamanote", url: "https://www.youtube.com/embed/1A2b3zW3m2A"},
+  {station: "新宿", line: "yamanote", url: "https://www.youtube.com/embed/K0kF4PKRMAE"},
+  {station: "渋谷", line: "yamanote", url: "https://www.youtube.com/embed/IH_hQylka-s"},
+  {station: "東京", line: "chuo", url: "https://www.youtube.com/embed/dQw4w9WgXcQ"},
+  {station: "御茶ノ水", line: "chuo", url: "https://www.youtube.com/embed/3JZ_D3ELwOQ"},
+  {station: "中野", line: "chuo", url: "https://www.youtube.com/embed/l482T0yNkeo"},
+  {station: "上野", line: "keihintohoku", url: "https://www.youtube.com/embed/mQgnBJgpo6g"},
+  {station: "秋葉原", line: "keihintohoku", url: "https://www.youtube.com/embed/ZZ5LpwO-An4"},
+  {station: "品川", line: "keihintohoku", url: "https://www.youtube.com/embed/9bZkp7q19f0"}
+];
